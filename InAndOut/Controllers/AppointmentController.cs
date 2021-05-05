93268e1302093340824a523fca14af1611444104
Abhijit Mondal Abhi <abhi@localhero.de>
2021-05-05 15:00:25 +0200

    Add project files.

---
+++ InAndOut/Controllers/AppointmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+            // var toDaysDate = DateTime.Now.ToShortDateString();
+
+            // return Ok(toDaysDate); 
+        }
+    }
+}
